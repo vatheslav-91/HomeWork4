c8b09af157aeedffe99bd5aff168261c768f1d2a
Вячеслав <vatheslav-91@mail.ru>
2022-08-02 01:29:11 +0500

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         int ios = 0;
         int android = 1;
@@ -11,5 +11,16 @@ else {
      if (clientOS == android)
                 System.out.println("Установите версию приложения для Android ссылке");
 }
+            int clientDeviceYear = 2014;
+            int age = 2015;
+            if (clientDeviceYear > age) {
+                System.out.println("Установите стандартную версию приложения для iOS по ссылке");
+            }
+            else {
+                if (clientDeviceYear < age){
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                }
+            }
+
 }
 }
