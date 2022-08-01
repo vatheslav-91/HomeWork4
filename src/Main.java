d5c4a04e7f59fae1a60847c4b80bb2736d2f0211
Вячеслав <vatheslav-91@mail.ru>
2022-08-02 01:06:46 +0500

    Задание 1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ios = 0;
+        int android = 1;
+        int clientOS = 0;
+        if (clientOS == ios) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+else {
+    if (clientOS == android)
+                System.out.println("Установите версию приложения для Android ссылке");
+}
+}
+}
