e2f18de4775c85832cbb3a0fd03ff92409bfaaa1
Вячеслав <vatheslav-91@mail.ru>
2022-08-02 15:52:44 +0500

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,15 @@
 public class Main {
     public static void main(String[] args){
 
-        int ios = 0;
-        int android = 1;
-        int clientOS = 0;
-        if (clientOS == ios) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        }
-else {
-     if (clientOS == android)
-                System.out.println("Установите версию приложения для Android ссылке");
-}
-            int clientDeviceYear = 2014;
-            int age = 2015;
-            if (clientDeviceYear > age) {
-                System.out.println("Установите стандартную версию приложения для iOS по ссылке");
-            }
-            else {
-                if (clientDeviceYear < age){
-                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-                }
+         int year = 2021;
+         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+             System.out.printf("Год %s високосный", year);
+         }else {
+             System.out.printf("Год %s не високосный",year);
+         }
+
+ }
             }
 
-}
-}
+
+
