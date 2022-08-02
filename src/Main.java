8dae229e3b8caebe85190a8e371cc73f8f3f0334
Вячеслав <vatheslav-91@mail.ru>
2022-08-02 16:27:14 +0500

    Задание 5

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-
         int deliveryDistance = 95;
         if (deliveryDistance < 20) {
             System.out.println("Доставка за 1 день");
@@ -11,6 +10,31 @@ public class Main {
         }
         if (deliveryDistance < 100) {
             System.out.println("Доставка за 3 дня");
+        }
+
+        int monthNumber = 12;
+        switch (monthNumber){
+            case 12:
+            case 1:
+            case 2:
+                    System.out.println("Зимний месяц");
+                    break;
+            case 3:
+            case 4:
+            case 5:
+                        System.out.println("Весений месяц");
+                        break;
+            case 6:
+            case 7:
+            case 8:
+                            System.out.println("Летний месяц");
+                            break;
+            case 9:
+            case 10:
+            case 11:
+                                System.out.println("Осенний месяц");
+            default:
+                System.out.println("Нет такого месяца");
         }
     }
 }
